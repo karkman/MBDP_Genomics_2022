e90867a3e5dfce9672406c26251a5f69b69d976e
TKSJ2020 <76162002+TKSJ2020@users.noreply.github.com>
2022-03-08 12:17:36 +0200

    Update kaiju.sh

--- Practicals/kaiju.sh
+++ Practicals/kaiju.sh
@@ -6,10 +6,13 @@
 #SBATCH --time=06:00:00
 #SBATCH --partition=small
 #SBATCH --ntasks=1
-#SBATCH --nodes=1  
+#SBATCH --nodes=1
 #SBATCH --cpus-per-task=4
 #SBATCH --account=project_2005590
 #SBATCH --mem=40000
+
+module load gcc/9.1.0
+module load fastx-toolkit
 
 
 # Kaiju database files
@@ -17,9 +20,10 @@ names=/scratch/project_2005590/databases/kaiju_database/names.dmp
 nodes=/scratch/project_2005590/databases/kaiju_database/nodes.dmp
 db=/scratch/project_2005590/databases/kaiju_database/kaiju_db.fmi
 
-# Cyano strain label and the Kaiju input path variable
-input=spades_327/scaffolds.fasta
-strain=327
+
+# Cyano strain input and output names
+input=spades_328/scaffolds.fasta
+strain=328
 
 # Run Kaiju
 kaiju -t $nodes -f $db -i $input -o strain_"$strain".kaiju.txt -z $SLURM_CPUS_PER_TASK
@@ -27,11 +31,7 @@ kaiju -t $nodes -f $db -i $input -o strain_"$strain".kaiju.txt -z $SLURM_CPUS_PE
 # Add taxon names (phylum level) to the first Kaiju output table (uses the previous output as input)
 kaiju-addTaxonNames -t $nodes -n $names -r phylum -i strain_"$strain".kaiju.txt -o strain_"$strain".kaiju.taxons.txt
 
-
-# Format the spades output using fastx-toolkit
-# Activate the fastx-toolkit module
-module load fastx-toolkit
-
+# Format the spades output fasta file
 fasta_formatter -i $input -o fmt.spades_$strain".fasta"
 
 # Use the Kaiju output table to select only the contigs classified as Cyanobacteria
